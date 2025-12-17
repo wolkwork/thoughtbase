c915e4246f70c44b7c641cdfaac6ea5457328ed0
Robert <robertbroersma@gmail.com>
2025-12-17 15:49:42 +0100

    kak

--- apps/app/src/lib/base-url.ts
+++ apps/app/src/lib/base-url.ts
@@ -3,5 +3,5 @@ import { env } from "~/env/server";
 export function getBaseUrl() {
   const protocol = env.VERCEL_ENV === "development" ? "http" : "https";
 
-  return `${protocol}://${env.VERCEL_ENV === "production" ? env.VERCEL_PROJECT_PRODUCTION_URL : env.VERCEL_BRANCH_URL}`;
+  return `${protocol}://${env.VERCEL_ENV === "production" ? "thoughtbase.app" : env.VERCEL_BRANCH_URL}`;
 }
