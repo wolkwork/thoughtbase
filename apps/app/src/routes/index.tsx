001ffe65f8570182142af755434ba3c57a1a561c
Robert <robertbroersma@gmail.com>
2025-12-17 14:20:07 +0100

    ???

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -4,4 +4,10 @@ export const Route = createFileRoute("/")({
   beforeLoad: async () => {
     throw redirect({ to: "/dashboard" });
   },
+  loader: () => {
+    return {
+      VERCEL_BRANCH_URL: process.env.VERCEL_BRANCH_URL,
+      VERCEL_ENV: process.env.VERCEL_ENV,
+    };
+  },
 });
