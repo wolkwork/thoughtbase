42ee68a2ac7042f897b6a84851bf33a058beda06
Robert <robertbroersma@gmail.com>
2026-01-13 16:21:15 +0100

    Remove top-level await

--- apps/app/src/lib/db/index.ts
+++ apps/app/src/lib/db/index.ts
@@ -1,22 +1,21 @@
 import { neon } from "@neondatabase/serverless";
 import { createServerOnlyFn } from "@tanstack/react-start";
 import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import { env } from "~/env/server";
 
 import * as schema from "~/lib/db/schema";
 
-const getDatabase = createServerOnlyFn(async () => {
+const getDatabase = createServerOnlyFn(() => {
   if (env.VERCEL === "1") {
     const client = neon(env.DATABASE_URL);
     return drizzleNeon({ client, schema, casing: "snake_case" });
   }
-
-  const { drizzle: drizzlePg } = await import("drizzle-orm/postgres-js");
-  const { default: postgres } = await import("postgres");
 
   const driver = postgres(env.DATABASE_URL);
 
   return drizzlePg({ client: driver, schema, casing: "snake_case" });
 });
 
-export const db = await getDatabase();
+export const db = getDatabase();
