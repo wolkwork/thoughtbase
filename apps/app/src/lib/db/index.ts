e9d893de7b105b346ab66f9f20b186371f1d3ed0
Robert <robertbroersma@gmail.com>
2026-01-13 23:09:58 +0100

    remove logs

--- apps/app/src/lib/db/index.ts
+++ apps/app/src/lib/db/index.ts
@@ -12,26 +12,18 @@ neonConfig.webSocketConstructor = ws;
 
 const getDatabase = createServerOnlyFn(() => {
   if (env.VERCEL === "1") {
-    console.time("[DB] neon");
     const client = new Pool({ connectionString: env.DATABASE_URL });
-    console.timeEnd("[DB] neon");
 
-    console.time("[DB] Alternative");
     drizzleNeon({ client, casing: "snake_case" });
-    console.timeEnd("[DB] Alternative");
 
-    console.time("[DB] drizzleNeon");
     const db = drizzleNeon({ client, schema, casing: "snake_case" });
-    console.timeEnd("[DB] drizzleNeon");
 
     return db;
   }
 
   const driver = postgres(env.DATABASE_URL);
 
-  console.time("[DB] postgres");
   const db = drizzlePg({ client: driver, schema, casing: "snake_case" });
-  console.timeEnd("[DB] postgres");
 
   return db;
 });
