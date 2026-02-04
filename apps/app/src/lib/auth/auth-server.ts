e6cdcae6d6f4c5da02f5714fbc1a1b6ab6bb30b9
Robert <robertbroersma@gmail.com>
2026-02-04 11:24:26 +0100

    oops

--- apps/app/src/lib/auth/auth-server.ts
+++ apps/app/src/lib/auth/auth-server.ts
@@ -3,9 +3,9 @@ import { env } from "~/env/server";
 
 export const { handler, getToken, fetchAuthQuery, fetchAuthMutation, fetchAuthAction } =
   convexBetterAuthReactStart({
-    convexUrl: env.VITE_CONVEX_URL,
+    convexUrl: env.CONVEX_URL,
     convexSiteUrl:
       env.VERCEL_ENV !== "development"
-        ? env.VITE_CONVEX_URL.replace(".convex.cloud", ".convex.site")
+        ? env.CONVEX_URL.replace(".convex.cloud", ".convex.site")
         : "http://127.0.0.1:3211",
   });
